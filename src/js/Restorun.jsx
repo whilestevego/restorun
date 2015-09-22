a23620ee3dc514131518bf4f92c1594b11cbc8b5
Steve Godin <steve.god@gmail.com>
2015-09-21 20:53:07 -0700

    Show basic map of Downtown Vancouver

--- src/js/Restorun.jsx
+++ src/js/Restorun.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { GoogleMap, Marker } from 'react-google-maps';
 
 export default class Restorun extends React.Component {
-    constructor() {
-        super();
-    }
+  state = {
+    center: {lat: 49.282225, lng: -123.122884}
+  };
 
-    render() {
-        return <div>
-          <p>Test</p>
-        </div>
-    }
+  render () {
+    return (
+      <GoogleMap
+        containerProps={{
+          ...this.props,
+          style: {
+            height: "500px",
+            width: "500px"
+          },
+        }}
+        ref="map"
+        defaultZoom={14}
+        defaultCenter={this.state.center}
+      >
+      </GoogleMap>
+    );
+  }
 }
