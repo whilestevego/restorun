1d7f82eaf5bfa5d55ef472973b55235f70062956
Steve Godin <steve.god@gmail.com>
2015-09-22 08:03:05 -0700

    Place markers for restaurant

--- src/js/Restorun.jsx
+++ src/js/Restorun.jsx
@@ -15,25 +15,55 @@ export default class Restorun extends React.Component {
     return new google.maps.LatLng(this.props.lat, this.props.lng);
   }
 
-  handleResize = (event) => {
-    this.setState({window: {width: window.innerWidth, height: window.innerHeight}});
+  placeMarker = (result) => {
+    let options = {
+      position: result.geometry.location,
+      map: this.state.map
+    }
+
+    new google.maps.Marker(options);
+  }
+
+  searchLocation = (location) => {
+    let request = {
+      location: location,
+      radius: '5000',
+      types: ['restaurant']
+    };
+
+    let service = new google.maps.places.PlacesService(this.state.map);
+    service.nearbySearch(request, this._handlePlacesSearch);
   };
 
   componentDidMount = () => {
+    let initMapCenter = this.mapCenterLatLng();
     let mapOptions = {
-      center: this.mapCenterLatLng(),
+      center: initMapCenter,
       zoom: this.props.initialZoom
     }
 
-    let map = new google.maps.Map(React.findDOMNode(this.refs.map), mapOptions);
+    this.state.map = new google.maps.Map(React.findDOMNode(this.refs.map), mapOptions);
+    this.searchLocation(initMapCenter);
 
-    this.setState({map: map});
-
-    window.addEventListener('resize', this.handleResize);
+    window.addEventListener('resize', this._handleResize);
   }
 
   componentWillUnmount = () => {
-    window.removeEventListener('resize', this.handleResize);
+    window.removeEventListener('resize', this._handleResize);
+  };
+
+  // EVENT HANDLERS
+  _handlePlacesSearch = (results, status) => {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (let i = 0; i < results.length; i++) {
+        let place = results[i];
+        this.placeMarker(place);
+      }
+    }
+  };
+
+  _handleResize = (event) => {
+    this.setState({window: {width: window.innerWidth, height: window.innerHeight}});
   };
 
   render () {
