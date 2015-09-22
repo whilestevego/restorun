730714a9228bd95aee89b991504eae5a18f43f2a
Steve Godin <steve.god@gmail.com>
2015-09-21 19:55:27 -0700

    Create starting point

---
+++ src/js/Restorun.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Restorun extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return <div>
+          <p>Test</p>
+        </div>
+    }
+}
---
+++ src/js/entry.js
@@ -0,0 +1,9 @@
+// Import CSS
+import '../css/master.scss';
+
+// Import React and JS
+import Restorun from './Restorun.jsx';
+import React from 'react';
+
+// Render!
+React.render(<Restorun />, document.getElementsByTagName('body')[0]);
